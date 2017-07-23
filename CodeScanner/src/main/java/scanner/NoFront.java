33394448419c8787d0c067b17379d0594b3a71c6
Administrador <Administrador@BANGHOB-QUOMD47>
2017-07-23 20:10:19 -0300

    Ordered and privatized methods

--- CodeScanner/src/main/java/scanner/NoFront.java
+++ CodeScanner/src/main/java/scanner/NoFront.java
@@ -1,4 +1,5 @@
 package scanner;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -37,12 +38,13 @@ import com.google.zxing.Reader;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
-//todo:add tests
+
 public class NoFront {
 	private static Logger logger;
 	private static Properties properties;
 	private static ResourceBundle messages;
 	private static Webcam webcam;
+	
 	public static void main(String[] args){	
 		String rom = "";		
 		webcam = Webcam.getDefault();
@@ -88,6 +90,17 @@ public class NoFront {
 		logger.info("----------");		
 		System.exit(0);
 	}
+
+	private static Properties loadProperties(String fileName) {
+		Properties props = new Properties();
+		try (InputStream input = new FileInputStream(fileName);) {			
+			props.load(input);
+		} catch (IOException ex) {
+			logger.info(ex.toString());
+			System.exit(1);
+		} 
+		return props;
+	}
 	
 	private static void setLanguage() {
 		String language = properties.getProperty("language");
@@ -99,8 +112,73 @@ public class NoFront {
 		Locale locale = new Locale(language,country);
 		messages = ResourceBundle.getBundle("messages", locale);
 	}	
+
+	private static boolean startWebCam() {
+		if (webcam!=null) {
+			webcam.open();
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	private static void showMessage() {
+		JFrame mainWindow = new JFrame("img/CodeScanner");
+		ImageIcon animatedGIF = new ImageIcon("img/scanning.gif");
+        JLabel labelForGIF = new JLabel(animatedGIF);	
+        JLabel textLabel = new JLabel(messages.getString("scanning"));
+		Border border = BorderFactory.createLineBorder(Color.WHITE, 2,false);		
+		labelForGIF.setOpaque(true);                
+		textLabel.setForeground(Color.WHITE);
+		textLabel.setBackground(Color.BLACK);
+		textLabel.setOpaque(true);		
+		textLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		textLabel.setVerticalAlignment(SwingConstants.CENTER);
+		textLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 18));        
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+        mainWindow.getContentPane().add(labelForGIF, BorderLayout.CENTER);
+		mainWindow.getContentPane().add(textLabel, BorderLayout.PAGE_END);
+		mainWindow.setBackground(Color.BLACK);
+		mainWindow.setUndecorated(true);	
+		mainWindow.getContentPane().setBackground(Color.BLACK);
+		mainWindow.pack();
+		mainWindow.setSize(133,100);
+		mainWindow.getRootPane().setBorder(border);
+		mainWindow.setLocationRelativeTo(null);
+		mainWindow.setIconImage(Toolkit.getDefaultToolkit().getImage("img/icon.png"));
+		mainWindow.setVisible(true);
+	}
+
+	private static String getCodeFromPicture() throws NotFoundException, ChecksumException, FormatException, IOException {
+		if(webcam==null) {
+			return mockRead().getText();
+		} else {
+			return realRead().getText();
+		}
+	}	
+
+	private static Result realRead() throws NotFoundException, ChecksumException, FormatException {
+		BufferedImage image = webcam.getImage();
+		LuminanceSource source = new BufferedImageLuminanceSource(image);
+		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+		Reader reader = new MultiFormatReader();
+		return reader.decode(bitmap);
+	}
 	
-	public static void launch(String rom) throws IOException {
+	private static Result mockRead() throws IOException, NotFoundException, ChecksumException, FormatException {
+		String mockImg = ".\\codes\\genesis\\Alien Soldier (Europe).png";
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		Map < DecodeHintType, Object > tmpHintsMap = new EnumMap (DecodeHintType.class);		
+		tmpHintsMap.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);		
+		tmpHintsMap.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+		BufferedImage image = ImageIO.read(new File(mockImg));
+		LuminanceSource source = new BufferedImageLuminanceSource(image);
+		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+		Reader reader = new MultiFormatReader();
+		return reader.decode(bitmap,tmpHintsMap);
+	}	
+	
+	private static void launch(String rom) throws IOException {
 		String nesRomsDir =  properties.getProperty("nesRomsDir");
 		String nesExec = properties.getProperty("nesExec");
 		String snesRomsDir = properties.getProperty("snesRomsDir");
@@ -118,86 +196,7 @@ public class NoFront {
 		}
 	}
 	
-	public static boolean fileExistsInDir(String file,String dir) {
+	private static boolean fileExistsInDir(String file,String dir) {
 		return new File(dir,file).exists();
 	}
-
-	public static Result realRead() throws NotFoundException, ChecksumException, FormatException {
-		BufferedImage image = webcam.getImage();
-		LuminanceSource source = new BufferedImageLuminanceSource(image);
-		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-		Reader reader = new MultiFormatReader();
-		return reader.decode(bitmap);
-	}
-	
-	public static Result mockRead() throws IOException, NotFoundException, ChecksumException, FormatException {
-		String mockImg = ".\\codes\\genesis\\Alien Soldier (Europe).png";
-		@SuppressWarnings({ "unchecked", "rawtypes" })
-		Map < DecodeHintType, Object > tmpHintsMap = new EnumMap (DecodeHintType.class);		
-		tmpHintsMap.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);		
-		tmpHintsMap.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
-		BufferedImage image = ImageIO.read(new File(mockImg));
-		LuminanceSource source = new BufferedImageLuminanceSource(image);
-		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-		Reader reader = new MultiFormatReader();
-		return reader.decode(bitmap,tmpHintsMap);
-	}	
-
-	public static void showMessage() {
-		JFrame mainWindow = new JFrame("img/CodeScanner");
-		ImageIcon animatedGIF = new ImageIcon("img/scanning.gif");
-        JLabel labelForGIF = new JLabel(animatedGIF);	
-        JLabel textLabel = new JLabel(messages.getString("scanning"));
-		Border border = BorderFactory.createLineBorder(Color.WHITE, 2,false);
-		
-		labelForGIF.setOpaque(true);        
-        
-		textLabel.setForeground(Color.WHITE);
-		textLabel.setBackground(Color.BLACK);
-		textLabel.setOpaque(true);		
-		textLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		textLabel.setVerticalAlignment(SwingConstants.CENTER);
-		textLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 18));        
-
-		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
-        mainWindow.getContentPane().add(labelForGIF, BorderLayout.CENTER);
-		mainWindow.getContentPane().add(textLabel, BorderLayout.PAGE_END);
-		mainWindow.setBackground(Color.BLACK);
-		mainWindow.setUndecorated(true);	
-		mainWindow.getContentPane().setBackground(Color.BLACK);
-		mainWindow.pack();
-		mainWindow.setSize(133,100);
-		mainWindow.getRootPane().setBorder(border);
-		mainWindow.setLocationRelativeTo(null);
-		mainWindow.setIconImage(Toolkit.getDefaultToolkit().getImage("img/icon.png"));
-		mainWindow.setVisible(true);
-	}
-
-	public static Properties loadProperties(String fileName) {
-		Properties props = new Properties();
-		try (InputStream input = new FileInputStream(fileName);) {			
-			props.load(input);
-		} catch (IOException ex) {
-			logger.info(ex.toString());
-			System.exit(1);
-		} 
-		return props;
-	}
-
-	public static boolean startWebCam() {
-		if (webcam!=null) {
-			webcam.open();
-		} else {
-			return false;
-		}
-		return true;
-	}
-
-	public static String getCodeFromPicture() throws NotFoundException, ChecksumException, FormatException, IOException {
-		if(webcam==null) {
-			return mockRead().getText();
-		} else {
-			return realRead().getText();
-		}
-	}	
 }
