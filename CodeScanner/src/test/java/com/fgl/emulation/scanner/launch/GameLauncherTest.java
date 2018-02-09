a32da156935436c5432bfa2e51fe7453852aed15
Administrador <Administrador@BANGHOB-QUOMD47>
2018-02-08 21:39:28 -0300

    Adding tests

--- CodeScanner/src/test/java/com/fgl/emulation/scanner/launch/GameLauncherTest.java
+++ CodeScanner/src/test/java/com/fgl/emulation/scanner/launch/GameLauncherTest.java
@@ -1,18 +1,44 @@
 package com.fgl.emulation.scanner.launch;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameLauncherTest {
+	GameLauncher gameLauncher;
+	
+	@Before
+	public void setupTest() throws IOException {
+		gameLauncher=new GameLauncher();
+		assertNotNull(gameLauncher);
+	}
+	
+	@After
+	public void finishTest() throws IOException {
+		gameLauncher=null;
+		assertNull(gameLauncher);
+	}
+	
 	@Test
-	public void testLaunch() {
-		GameLauncher gameLauncher = new GameLauncher();
+	public void testLaunch() throws IOException {
 		try {
-			gameLauncher.launch("sadsa");
+			assertFalse(gameLauncher.launch("someGame"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw e;
 		}
 	}
+	
+	@Test
+	public void testAddAndGetFoldersAndExec() throws IOException {
+		gameLauncher.addFolderAndExec("someFolder","someExec");
+		String exec = gameLauncher.getFoldersAndExecs().get("someFolder");
+		assertTrue("someExec".equals(exec));
+	}	
 }
